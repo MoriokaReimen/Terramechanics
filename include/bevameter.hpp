3489873c00e7a72bf48074522095d79d20f152e3
MoriokaReimen <menschenjager.mark.neun@gmail.com>
2014-12-08 10:55:51 +0900

    mod bevameter

--- include/bevameter.hpp
+++ include/bevameter.hpp
@@ -1,5 +1,5 @@
 #include<cmath>
 
 namespace bevameter {
-double getShear(double M, double r_i, double r_o);
+double getShear(const double& M, const double& r_i, const double& r_o);
 }
--- src/bevameter.cpp
+++ src/bevameter.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using std::pow;
 #define M_PI 3.14
-double bevameter::getShear(double M, double r_i = 5.0, double r_o = 10.0)
+double bevameter::getShear(const double& M, const double& r_i = 5.0, const double& r_o = 10.0)
 {
   double shear = 1.50 / (pow(r_o, 3) - pow(r_i, 3)) / M_PI;
   return shear;
