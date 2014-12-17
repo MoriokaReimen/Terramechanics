a46490ec61b87704de7214bf1ddfe56710dde893
Kei Nakata <menschenjager.mark.neun@gmail.com>
2014-12-17 21:45:55 +0900

    indent codes

--- include/bevameter.hpp
+++ include/bevameter.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include<cmath>
 
-namespace bevameter {
+namespace bevameter
+{
 double getShear(const double& M, const double& r_i, const double& r_o);
 }
--- include/wheel_soil.hpp
+++ include/wheel_soil.hpp
@@ -42,27 +42,25 @@ using std::function;
 *    @struct Soil
 *    @brief parameters for Soil
 */
-struct Soil
-{
-    double phi{0.0};//! angle of internal shearing resistance
-    double c{298.123};                //! cohesion[Pa]
-    double K{0.00068509};             //! shear deformation modulus[m]
-    double k1{0.141e6};              //! pressure sinkage modulus 1
-    double k2{0.0};                    //! pressure sinkage modulus 2
-    double n{0.89};                   //! exponent of sinkage to width ratio
-    double gamma{0.0575};             //! density[N/m^3]
+struct Soil {
+    double phi {0.0}; //! angle of internal shearing resistance
+    double c {298.123};               //! cohesion[Pa]
+    double K {0.00068509};            //! shear deformation modulus[m]
+    double k1 {0.141e6};             //! pressure sinkage modulus 1
+    double k2 {0.0};                   //! pressure sinkage modulus 2
+    double n {0.89};                  //! exponent of sinkage to width ratio
+    double gamma {0.0575};            //! density[N/m^3]
 };
 
 /*
 *    @struct Wheel
 *    @brief parameters for wheel
 */
-struct Wheel
-{
+struct Wheel {
     /* Wheel properties */
-    double r{0.13};                    //! radius[m]
-    double b{0.16};                    //! width[m]
-    double w{135};                    //! vertical axle load[N]
+    double r {0.13};                   //! radius[m]
+    double b {0.16};                   //! width[m]
+    double w {135};                   //! vertical axle load[N]
 };
 
 /*
@@ -76,13 +74,13 @@ private:
     Wheel wheel_;
     double getSigma1_(const double& theta, const double& theta1) const;
     double getSigma2_(const double& theta, const double& theta1, const double& theta2,
-            const double& theta_m) const;
+                      const double& theta_m) const;
 public:
     WheelSoil(const Soil& soil, const Wheel& wheel);
     double getSigma(const double& theta, const double& theta1, const double& theta2,
-        const double& theta_m) const;
+                    const double& theta_m) const;
     double getTau(const double& theta, const double& theta1, const double& theta2,
-        const double& theta_m, const double& slip) const;
+                  const double& theta_m, const double& slip) const;
 };
 
 double integrate(function<double(double)> func, double start, double end);
