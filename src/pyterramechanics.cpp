a24c312101a3c77dd565e20e4d8418ae3be5a4a3
Kei Nakata <menschenjager.mark.neun@gmail.com>
2014-12-26 23:35:57 +0900

    add wheel_soil functions

--- src/pyterramechanics.cpp
+++ src/pyterramechanics.cpp
@@ -35,6 +35,7 @@
 */
 #include "bevameter.hpp"
 #include "angle.hpp"
+#include "wheel_soil.hpp"
 #include <boost/python.hpp>
 using namespace boost::python;
 
@@ -47,6 +48,30 @@ BOOST_PYTHON_MODULE(pyterramechanics)
   /* define angle manupulation functions */
   def("degToRad", degToRad);
   def("radToDeg", radToDeg);
+
+  /* define wheel soil functions */
+  class_<Soil>("Soil")
+    .def_readwrite("phi", &Soil::phi)
+    .def_readwrite("c", &Soil::phi)
+    .def_readwrite("K", &Soil::phi)
+    .def_readwrite("k1", &Soil::phi)
+    .def_readwrite("k2", &Soil::phi)
+    .def_readwrite("n", &Soil::phi)
+    .def_readwrite("gamma", &Soil::phi);
+
+
+  class_<Wheel>("Wheel")
+    .def_readwrite("r", &Wheel::r)
+    .def_readwrite("b", &Wheel::b)
+    .def_readwrite("w", &Wheel::w);
+
+
+  class_<WheelSoil>("WheelSoil", init<Soil, Wheel>())
+    .def("getSigma", &WheelSoil::getSigma)
+    .def("getTau", &WheelSoil::getTau)
+    .def("getTraction", &WheelSoil::getTraction)
+    .def("getTorque", &WheelSoil::getTorque)
+    .def("getDrawbar", &WheelSoil::getDrawbar);
 
   /* define bevameter functions */
   {
